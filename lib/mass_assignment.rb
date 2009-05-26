98b530270a0e15cb6195fa67a4a79d123652c519
Lance Ivy <lance@cainlevy.net>
2009-05-26 16:58:16 -0700

    configurable mass assignment policies. these are default whitelists/blacklists/patterns that may be overridden by #assign(). you may set a global policy on ActiveRecord::Base!

--- lib/mass_assignment.rb
+++ lib/mass_assignment.rb
@@ -1,16 +1,61 @@
 module MassAssignment
-  def assign(new_attributes, allowed_attributes = nil)
+  def self.included(base)
+    base.class_eval do extend ClassMethods end
+  end
+
+  def assign(attributes, allowed_attributes = nil)
     if allowed_attributes
-      attributes = new_attributes.stringify_keys if new_attributes
-      allowed_attributes = allowed_attributes.map(&:to_s)
-      
-      # filter
-      safe_attributes = attributes.reject { |k, v| !allowed_attributes.include?(k.gsub(/\(.+/, "")) }
-      
-      # assign
-      self.send("attributes=", safe_attributes, false) # assign w/o attr_protected/attr_accessible
+      safe_attributes = filter_attributes(attributes, :only => allowed_attributes)
+      self.send("attributes=", safe_attributes, false)
     else
-      self.attributes = new_attributes
+      if policy = self.class.get_mass_assignment_policy
+        safe_attributes = filter_attributes(attributes, policy)
+        self.send("attributes=", safe_attributes, false)
+      else
+        # backwards compatibility. use attr_protected and attr_accessible.
+        self.attributes = attributes
+      end
+    end
+  end
+  
+  private
+
+  def filter_attributes(attributes, options = {}) # could surely be refactored.
+    attributes = attributes.stringify_keys
+  
+    if options[:only]
+      if options[:only].is_a? Regexp
+        attributes.reject { |k, v| !k.gsub(/\(.+/, "").match(options[:only]) }
+      elsif options[:only] == :all
+        attributes
+      else
+        whitelist = options[:only].map(&:to_s)
+        attributes.reject { |k, v| !whitelist.include?(k.gsub(/\(.+/, "")) }
+      end
+    elsif options[:except]
+      if options[:except].is_a? Regexp
+        attributes.reject { |k, v| k.gsub(/\(.+/, "").match(options[:except]) }
+      elsif options[:except] == :all
+        {}
+      else
+        blacklist = options[:except].map(&:to_s)
+        attributes.reject { |k, v| blacklist.include?(k.gsub(/\(.+/, "")) }
+      end
+    else
+      attributes
+    end
+  end
+  
+  module ClassMethods
+    # sets a default mass assignment policy for your model's attributes. you may choose to start from a
+    # closed state that allows no mass assignment, an open state that allows any mass assignment (this is
+    # activerecord's default), or somewhere inbetween.
+    def mass_assignment_policy(val)
+      write_inheritable_attribute :mass_assignment_policy, val
+    end
+    
+    def get_mass_assignment_policy
+      read_inheritable_attribute :mass_assignment_policy
     end
   end
 end
--- test/mass_assignment_test.rb
+++ test/mass_assignment_test.rb
@@ -1,22 +1,36 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class User < ActiveRecord::Base
-  attr_protected :admin
-
   class << self
     def columns
       @columns ||= [
         ActiveRecord::ConnectionAdapters::Column.new("name", nil, "varchar(100)", true),
         ActiveRecord::ConnectionAdapters::Column.new("email", nil, "varchar(100)", true),
-        ActiveRecord::ConnectionAdapters::Column.new("admin", nil, "tinyint(1)", false)
+        ActiveRecord::ConnectionAdapters::Column.new("role_id", nil, "integer(11)", false)
       ]
     end
   end
 end
 
+class ProtectedUser < User
+  attr_protected :role_id
+end
+
+class ClosedUser < User
+  mass_assignment_policy :except => :all
+end
+
+class SemiClosedUser < User
+  mass_assignment_policy :except => /_id$/
+end
+
+class OpenUser < User
+  mass_assignment_policy :only => :all
+end
+
 class MassAssignmentTest < ActiveSupport::TestCase
   def setup
-    @user = User.new
+    @user = ProtectedUser.new
     @attributes = {"name" => "Bob", "email" => "bob@example.com"}
     ActiveRecord::Base.logger = stub('debug' => true)
   end
@@ -28,20 +42,69 @@ class MassAssignmentTest < ActiveSupport::TestCase
   end
   
   test "assigning protected attributes" do
-    @user.assign(@attributes.merge(:admin => true))
+    @user.assign(@attributes.merge(:role_id => 1))
     assert_equal "Bob", @user.name
-    assert !@user.admin?
+    assert_nil @user.role_id
   end
   
-  test "bypassing protected attributes" do
-    @user.assign(@attributes.merge(:admin => true), [:name, :email, :admin])
+  test "overriding protected attributes" do
+    @user.assign(@attributes.merge(:role_id => 1), [:name, :email, :role_id])
     assert_equal "Bob", @user.name
-    assert @user.admin?
+    assert_equal 1, @user.role_id
   end
   
   test "assigning unallowed attributes" do
-    @user.assign(@attributes.merge(:admin => true), [:name, :email])
+    @user.assign(@attributes.merge(:role_id => 1), [:name, :email])
     assert_equal "Bob", @user.name
-    assert !@user.admin?
+    assert_nil @user.role_id
+  end
+end
+
+class MassAssignmentPolicyTest < ActiveSupport::TestCase
+  def setup
+    @attributes = {"name" => "Bob", "role_id" => 1}
+    ActiveRecord::Base.logger = stub('debug' => true)
+  end
+  
+  test "an open policy" do
+    @user = OpenUser.new
+    @user.assign(@attributes)
+    assert_equal "Bob", @user.name
+    assert_equal 1, @user.role_id
+  end
+  
+  test "an overridden open policy" do
+    @user = OpenUser.new
+    @user.assign(@attributes, [:name])
+    assert_equal "Bob", @user.name
+    assert_nil @user.role_id
+  end
+  
+  test "a closed policy" do
+    @user = ClosedUser.new
+    @user.assign(@attributes)
+    assert_nil @user.name
+    assert_nil @user.role_id
+  end
+  
+  test "an overridden closed policy" do
+    @user = ClosedUser.new
+    @user.assign(@attributes, [:name, :role_id])
+    assert_equal "Bob", @user.name
+    assert_equal 1, @user.role_id
+  end
+  
+  test "a semi-closed policy" do
+    @user = SemiClosedUser.new
+    @user.assign(@attributes)
+    assert_equal "Bob", @user.name
+    assert_nil @user.role_id
+  end
+  
+  test "an overridden semi-closed policy" do
+    @user = SemiClosedUser.new
+    @user.assign(@attributes, [:name, :role_id])
+    assert_equal "Bob", @user.name
+    assert_equal 1, @user.role_id
   end
 end
